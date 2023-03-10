7ef68f48832f1b3272d50aea2e2e4b0caa9aa19b
Chiamaka Ikeanyi <chiamakaikeanyi@gmail.com>
2023-03-09 18:43:01 -0800

    Prints the result of an addition

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-     """Sum function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a + b
-    """
+    """Print some of 1 and 2"""
     from add_0 import add
 
     a = 1
